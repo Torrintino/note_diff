ea1fcffa5b79300e66e6d0fe7be55bbc910dbae6
Lennart Weiss <weislenn@hu-berlin.de>
2018-12-12 09:28:37 +0100

    Added loop, so it only needs to be executed once

--- note_diff.py
+++ note_diff.py
@@ -5,14 +5,15 @@ import random
 notes = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D',
          'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab']
 
-a = random.randrange(12)
-b = a
-while a == b:
-    b = random.randrange(12)
-answer = int(input('How many steps do you need from {} to {}?\n'.format(
-    notes[a], notes[b])), 10)
-if (notes[b] == notes[(a + answer) % 12] or
-    notes[b] == notes[(a - answer) % 12]):
-    print('Correct.\n')
-else:
-    print('False.\n')
+while True:
+    a = random.randrange(12)
+    b = a
+    while a == b:
+        b = random.randrange(12)
+        answer = int(input('How many steps do you need from {} to {}?\n'.format(
+            notes[a], notes[b])), 10)
+        if (notes[b] == notes[(a + answer) % 12] or
+            notes[b] == notes[(a - answer) % 12]):
+            print('Correct.\n')
+        else:
+            print('False.\n')
